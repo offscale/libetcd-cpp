20db6e3a941c421e3d50d1ad5dd66197bd8f7e4c
Raed Romanov <raid_r@mail.ru>
2019-05-06 19:47:24 +0300

    Set vcpkg ref and hash

--- vcpkg/ports/etcdpp/portfile.cmake
+++ vcpkg/ports/etcdpp/portfile.cmake
@@ -5,11 +5,11 @@ include(vcpkg_common_functions)
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@github.com:SamuelMarks/libetcd-cpp.git
-    REF master
-    SHA512 b04f332a9ac8cc88a9d551ba16ff1d526abb686cc1a7ffc51f64d1ba6bf229ad7eb232fd2e6c9d3fdcecc5e23e3a5de69d4ec76a65d300bede4872e2f48622f8
+    REF 65a2fe55d427fb6511c419e34b607b57d667ec67
+    SHA512 db5aa4365eadbe3ff9f1549a35162c197fa821f4bf759a568694747aed1dda4902852a2472f22bb43e2ffea48698653e94be18503e9ac55691a8421ce8e23e7b
 )
 
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
+# file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
