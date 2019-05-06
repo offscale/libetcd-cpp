65a2fe55d427fb6511c419e34b607b57d667ec67
Raed Romanov <raid_r@mail.ru>
2019-05-06 19:43:33 +0300

    Packing new version of etcd protocol

--- vcpkg/ports/etcdpp/portfile.cmake
+++ vcpkg/ports/etcdpp/portfile.cmake
@@ -6,8 +6,10 @@ vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@github.com:SamuelMarks/libetcd-cpp.git
     REF master
-    SHA512 5cde86d103586bd95113cf6a1c0700cac5b4e1fb2d6f8af8eebba92e614e3f70a842affb6ed873735e94d4e16d66528161c98a65e5425866a2033ed513f3fb93
+    SHA512 b04f332a9ac8cc88a9d551ba16ff1d526abb686cc1a7ffc51f64d1ba6bf229ad7eb232fd2e6c9d3fdcecc5e23e3a5de69d4ec76a65d300bede4872e2f48622f8
 )
+
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
